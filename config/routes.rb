14fb0121e3f3655a2ea3bb8476ecc1968e4b2e72
Risa Harada <supikateriburu@gmail.com>
2017-12-26 16:53:24 +0900

    :put_litter_in_its_place: Remove blank lines & comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'companies/new'
-
   root 'pages#index'
-
   get 'pages/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
