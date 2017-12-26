e7a92216c6bab0bfe393719073a71af75ff71d7f
Risa Harada <supikateriburu@gmail.com>
2017-12-26 14:58:07 +0900

    :sparkles: Route devise/sessions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'companies/new'
 
-  devise_for :users
   root 'pages#index'
 
   get 'pages/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
