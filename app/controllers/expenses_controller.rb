d033c7336afcd28194b857745f63f6d6c8df4390
Risa Harada <supikateriburu@gmail.com>
2017-12-27 12:33:58 +0900

    :art: Add expenses controllers

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,26 @@
+class ExpensesController < ApplicationController
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user = current_user
+    if @expense.save
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :companies, only: [:new, :create]
+  resources :expenses, only: [:new, :create, :show]
   root 'pages#index'
   get 'pages/show'
 end
