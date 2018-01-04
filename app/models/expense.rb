72464c98508d47677195c916e3958c1edde64f5a
Risa Harada <supikateriburu@gmail.com>
2018-01-04 19:10:35 +0900

    :sparkles: Add  buttons and status on each expense
    承認ボタンとステータス表示を追加しました

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -15,4 +15,17 @@ class Expense < ApplicationRecord
     category9: 9,
     category10: 10
   }
+
+  def status
+    expense = self
+    if expense.approved_at.nil? && expense.rejected_at.nil?
+      return :pending
+    elsif !expense.approved_at.nil? && expense.rejected_at.nil?
+      return :approved
+    elsif expense.approved_at.nil? && !expense.rejected_at.nil?
+      return :rejected
+    else
+      return :error
+    end
+  end
 end
