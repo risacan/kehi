70193f70394229ae1973ac6b2355175f8ffe8f5e
Risa Harada <supikateriburu@gmail.com>
2018-01-09 19:20:36 +0900

    :recycle: Refactor test
    以下を参考にテストをDRYにしました
    https://github.com/kufu/kehi/pull/22#discussion_r160360098
    https://qiita.com/kuboon/items/a3726dd865702f6f7e35

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,25 +1,34 @@
 require "rails_helper"
 
 describe Expense, type: :model do
-  context "#status" do
-    example "approved_atのみpresentのとき、statusは:approvedを返す" do
-      approved_expense = FactoryBot.create(:expense, approved_at: Time.zone.now, rejected_at: nil)
-      expect(approved_expense.status).to eq :approved
+  subject(:expense) { FactoryBot.build(:expense, approved_at: approved_at, rejected_at: rejected_at) }
+
+  describe "#status" do
+    subject { expense.status }
+    context  "approved_atのみpresentのとき、statusは:approvedを返す" do
+      let(:approved_at) { Time.zone.now }
+      let(:rejected_at) { nil }
+      it { is_expected.to eq :approved}
     end
 
-    example "rejected_atのみpresentのとき、statusは:rejectedを返す" do
-      rejected_expense = FactoryBot.create(:expense, approved_at: nil, rejected_at: Time.zone.now)
-      expect(rejected_expense.status).to eq :rejected
+    context "rejected_atのみpresentのとき、statusは:rejectedを返す" do
+      let(:approved_at) { nil }
+      let(:rejected_at) { Time.zone.now }
+      it { is_expected.to eq :rejected }
     end
 
-    example "approved_atもrejected_atもnilのとき、statusは:pendingを返す" do
-      pending_expense = FactoryBot.create(:expense, approved_at: nil, rejected_at: nil)
-      expect(pending_expense.status).to eq :pending
+    context "approved_atもrejected_atもnilのとき、statusは:pendingを返す" do
+      let(:approved_at) { nil }
+      let(:rejected_at) { nil }
+      it { is_expected.to eq :pending }
     end
+  end
 
-    example "approved_atもrejected_atもpresentのとき、invalidになる" do
-      error_expense = FactoryBot.build(:expense, approved_at: Time.zone.now, rejected_at: Time.zone.now)
-      expect(error_expense).to be_invalid
+  describe "validation" do
+    context "approved_atもrejected_atもpresentのとき、invalidになる" do
+      let(:approved_at) { Time.zone.now }
+      let(:rejected_at) { Time.zone.now }
+      it { is_expected.to be_invalid }
     end
   end
 end
