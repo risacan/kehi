429993ce2185431269d65c30e98fcadbee1426d2
Risa Harada <supikateriburu@gmail.com>
2018-01-16 19:40:32 +0900

    :new: Add detail page for this months summary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def index
     @expenses = current_user.expenses.order(created_at: :desc).first(5)
-    @count_by_category = current_user.company.expenses.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
-    @pie_chart = current_user.company.expenses.approved.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @count_by_category = current_user.company.expenses.this_month.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @pie_chart = current_user.company.expenses.this_month.approved.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
   end
 
   def show
+    @count_by_category = current_user.company.expenses.this_month.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @pie_chart = current_user.company.expenses.this_month.approved.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
   end
 end
