9ec516f76f1001fd738e634b7e1e24276f89d067
Risa Harada <supikateriburu@gmail.com>
2018-01-16 16:52:46 +0900

    :bar_chart: Add random chart on toppage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def index
     @expenses = current_user.expenses.order(created_at: :desc).first(5)
+    @count_by_category = current_user.company.expenses.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @pie_chart = current_user.company.expenses.approved.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
   end
 
   def show
