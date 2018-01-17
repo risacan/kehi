373af75d0017051a82a1bbd02878ca1e86ff0643
Risa Harada <supikateriburu@gmail.com>
2018-01-17 14:24:44 +0900

    :art: Change graphs of toppage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def index
     @expenses = current_user.expenses.order(created_at: :desc).first(5)
-    @count_by_category = current_user.company.expenses.this_month.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
-    @pie_chart = current_user.company.expenses.this_month.approved.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @amount_by_category = current_user.company.expenses.this_month.group(:category).sum(:amount).map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
+    @number_of_category = current_user.company.expenses.this_month.group(:category).count.map{|k,v| [I18n.t(k, scope: "enums.expense.category"), v]}.to_h
   end
 
   def show
