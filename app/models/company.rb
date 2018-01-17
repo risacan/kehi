0fcc24d099e781324309cdf4aff51ef915360a01
Risa Harada <supikateriburu@gmail.com>
2018-01-17 19:11:10 +0900

    :shower: Fix indent

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,8 @@
 class Company < ApplicationRecord
-    has_many :users
-    validates :name, presence: true
+  has_many :users
+  validates :name, presence: true
 
-    def expenses
-        Expense.where(user: users)
-    end
+  def expenses
+    Expense.where(user: users)
+  end
 end
