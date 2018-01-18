80d47269065be80aec4a29c983f8f67b0656f25c
Risa Harada <supikateriburu@gmail.com>
2018-01-18 14:00:56 +0900

    :art: Change design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,9 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require chartkick
+
+$(document).on('ready turbolinks:load', function() {
+  $('.js-expense-panel').on('click', function() {
+    window.location = $(this).data('detail-url');
+  });
+});
