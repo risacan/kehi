59d1f27345aad35bdf87b9baa78845dce1b10b54
Risa Harada <supikateriburu@gmail.com>
2018-01-19 16:30:04 +0900

    :art: Change summary page
    * 共通部分は共通のviewをつかう
    * 従業員権限の人がアクセスした場合はroot_urlへリダイレクトする

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,19 @@ class PagesController < ApplicationController
   def index
     @my_latest_expenses = current_user.expenses.order(created_at: :desc).first(10)
     @company_latest_expenses = company_expenses.order(created_at: :desc).first(10)
-    @my_expenses = current_user.expenses
+    @my_expenses = my_expenses
     @company_expenses = company_expenses
     @amount_by_category = amount_by_category
     @number_of_category = number_of_category
   end
 
   def show
+    redirect_to root_url unless current_user.admin?
     @amount_by_category = amount_by_category
     @amount_by_category_last_month = amount_by_category_last_month
     @number_of_category = number_of_category
+    @company_expenses = company_expenses
+    @company_expenses_last_month = company_expenses.this_month(Time.zone.now.last_month)
   end
 
   private
@@ -30,5 +33,9 @@ class PagesController < ApplicationController
 
   def company_expenses
     current_user.company.expenses.countable
+  end
+
+  def my_expenses
+    current_user.expenses.countable
   end
 end
