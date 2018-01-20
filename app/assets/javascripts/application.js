a8af4d1433375dcc6680e48bbf18ac9f97361939
Risa Harada <supikateriburu@gmail.com>
2018-01-20 17:31:29 +0900

    :art: Enable submit after selecting approve/reject

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,20 @@ $(document).on('ready turbolinks:load', function() {
   $(document).on('click', '.js-expense-panel', function() {
     window.location = $(this).data('detail-url');
   });
+
+  $(document).on("change", "#radio_approve", function(){
+    if ( $(this).is(":checked")) {
+      $(".js-submit-button").prop("disabled", false)
+    } else {
+      $(".js-submit-button").prop("disabled", true)
+    }
+  })
+
+  $(document).on("change", "#radio_reject", function(){
+    if ( $(this).is(":checked")) {
+      $(".js-submit-button").prop("disabled", false)
+    } else {
+      $(".js-submit-button").prop("disabled", true)
+    }
+  })
 });
