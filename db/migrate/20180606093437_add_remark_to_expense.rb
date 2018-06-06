cc216774b76a72612a35d2436851689318292b5d
Kana Igarashi <kana.igarashi@smarthr.co.jp>
2018-06-06 19:21:21 +0900

    Add Remark column to expense

--- db/migrate/20180606093437_add_remark_to_expense.rb
+++ db/migrate/20180606093437_add_remark_to_expense.rb
@@ -1,4 +1,4 @@
-class AddColumnsToExpense < ActiveRecord::Migration[5.1]
+class AddRemarkToExpense < ActiveRecord::Migration[5.1]
   def change
     add_column :expenses, :remark, :text, after: :category
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111092703) do
+ActiveRecord::Schema.define(version: 20180606093437) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180111092703) do
     t.text "purpose"
     t.bigint "user_id"
     t.integer "category"
+    t.text "remark"
     t.datetime "paid_at"
     t.string "paid_to"
     t.integer "amount"
