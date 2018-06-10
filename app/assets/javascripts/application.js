7bd332a250275f43e6948c8bbb9d141dc0b4e4de
Risa Harada <supikateriburu@gmail.com>
2018-06-11 00:31:05 +0900

    :shower: Fix lint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,24 +3,24 @@
 //= require rails-ujs
 //= require chartkick
 
-$(document).on('ready turbolinks:load', function() {
-  $(document).on('click', '.js-expense-panel', function() {
-    window.location = $(this).data('detail-url');
+$(document).on("ready turbolinks:load", function() {
+  $(document).on("click", ".js-expense-panel", function() {
+    window.location = $(this).data("detail-url");
   });
 
-  $(document).on("change", "#radio_approve", function(){
-    if ( $(this).is(":checked")) {
-      $(".js-submit-button").prop("disabled", false)
+  $(document).on("change", "#radio_approve", function() {
+    if ($(this).is(":checked")) {
+      $(".js-submit-button").prop("disabled", false);
     } else {
-      $(".js-submit-button").prop("disabled", true)
+      $(".js-submit-button").prop("disabled", true);
     }
-  })
+  });
 
-  $(document).on("change", "#radio_reject", function(){
-    if ( $(this).is(":checked")) {
-      $(".js-submit-button").prop("disabled", false)
+  $(document).on("change", "#radio_reject", function() {
+    if ($(this).is(":checked")) {
+      $(".js-submit-button").prop("disabled", false);
     } else {
-      $(".js-submit-button").prop("disabled", true)
+      $(".js-submit-button").prop("disabled", true);
     }
-  })
+  });
 });
